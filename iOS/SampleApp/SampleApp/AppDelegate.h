d12b58bc6dd88d20e6ff413711a13c6689285955
Avneet Singh <avneet723@gmail.com>
2014-05-24 15:49:57 -0700

    .DS_Store banished

---
+++ iOS/SampleApp/SampleApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SampleApp
+//
+//  Created by Avneet Singh on 5/24/14.
+//  Copyright (c) 2014 Pivot.IO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/SampleApp/SampleApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by Avneet Singh on 5/24/14.
+//  Copyright (c) 2014 Pivot.IO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
