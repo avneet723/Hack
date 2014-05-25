b989aae03d9c2b8d4e5be85bbdaaacff2f159043
Avneet Singh <avneet723@gmail.com>
2014-05-25 15:10:21 -0700

    Initial Commit

---
+++ iOS/PlayingCards/PlayingCards/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PlayingCards
+//
+//  Created by Avneet Singh on 5/25/14.
+//  Copyright (c) 2014 Pivot.IO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
